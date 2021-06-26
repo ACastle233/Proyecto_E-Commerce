f20bb49c8a3693b6bddf0fc673d8d524be19b5a2
ACastle233 <ilartcastilo@gmail.com>
2021-06-26 12:31:40 -0500

    Funcion de API de ML

--- index.js
+++ index.js
@@ -30,21 +30,26 @@ function renderData(mercadoLibreObject) {
       .then((resp) => resp.json())
       .then((data) => {
         console.log(data);
-        let legend = document.createElement('h2');
-        legend.textContent = `${data.value}`;
-  
-        let createChuck = document.createElement('img');
-        createChuck.setAttribute('src', data.icon_url);
-        createChuck.style.width = '200px';
-  
-        textML.appendChild(legend)
-        textML.appendChild (createChuck)
+
+        for (let index = 0; index < 10; index++) {
+          let keywordMl = document.createElement('h2');
+          keywordMl.textContent = `${data[index].keyword}`;
+    
+          let createMLUrl = document.createElement('h3');
+          createMLUrl.textContent = `${data[index].url}`;
+    
+          textML.appendChild(keywordMl)
+          textML.appendChild (createMLUrl)
+          
+        }
+
+        
       })
       .catch((err) => {
         console.log(err);
     })
   }
   
-  const newMercadoLibre = new MercadoLibreService('https://api.mercadolibre.com/categories/' + 'MLA1743');
+  const newMercadoLibre = new MercadoLibreService('https://api.mercadolibre.com/trends/MLA/MLA1743');
   
   renderData(newMercadoLibre);
